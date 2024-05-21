67002913c093a3b2c58d3667505813eb9a53ba10
Pablo Rial Sartirana <65549425+rialpablojhotmailcom@users.noreply.github.com>
2024-05-21 13:40:55 +0100

    deploying issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,14 @@ import react from "@vitejs/plugin-react";
 // dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "./",
+//   server: { open: "/Punk-API" },
+// });
+
 export default defineConfig({
+  base: "/Punk-API/",
   plugins: [react()],
-  base: "./",
-  server: { open: "/Punk-API" },
 });
